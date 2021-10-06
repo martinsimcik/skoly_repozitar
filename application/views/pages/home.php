22bf0f93e0dfcd8166773a132d34f68c3d022463
martinsimcik <martin.simcik@atlas.cz>
2021-10-06 18:50:18 +0200

    data_and_filtrace

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -7,36 +7,100 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <style>
+            #myInput {
+  background-image: url('/css/searchicon.png'); /* Add a search icon to input */
+  background-position: 10px 12px; /* Position the search icon */
+  background-repeat: no-repeat; /* Do not repeat the icon image */
+  width: 100%; /* Full-width */
+  font-size: 16px; /* Increase font-size */
+  padding: 12px 20px 12px 40px; /* Add some padding */
+  border: 1px solid #ddd; /* Add a grey border */
+  margin-bottom: 12px; /* Add some space below the input */
+}
+
+#myTable {
+  border-collapse: collapse; /* Collapse borders */
+  width: 100%; /* Full-width */
+  border: 1px solid #ddd; /* Add a grey border */
+  font-size: 18px; /* Increase font-size */
+}
+
+#myTable th, #myTable td {
+  text-align: left; /* Left-align text */
+  padding: 12px; /* Add padding */
+}
+
+#myTable tr {
+  /* Add a bottom border to all table rows */
+  border-bottom: 1px solid #ddd;
+}
+
+#myTable tr.header, #myTable tr:hover {
+  /* Add a grey background color to the table header and on hover */
+  background-color: #f1f1f1;
+}
+        </style>
+        <script>
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
     </head>
     <body>
         
-        <?php
-
-include "dbConn.php"; // Using database connection file here
-
-$records = mysqli_query($db,"select * from skola");
-
-
-?>
+        
   <div class="container" class="text-center">
-<h1>Seznam zákazníků</h1>
-
-
+<h1>Seznam škol v ČR</h1>
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Hledejte..">  
         
-    <?php while($data = mysqli_fetch_array($records))
-{
-?>
         
-     <div class="col">   
-    <div class="card" style="width: 30rem; height: 2rem;">
-            <h4><?php echo $data['nazev']; ?></h4>
+        <table id="myTable">
+            <tr>
+                <th>Název školy</th>
+                <th>Město</th>
+                <th>Počet přijatých</th>
+                <th>Obor</th>
+            </tr>
+            <?php
+            $conn = mysqli_connect("localhost","root","","skoly");
+            $sql ="SELECT skola.nazev as 'Škola', mesto.nazev as 'Město', pocet_prijatych.pocet as 'Počet přijatých', obor.nazev as 'Obor' from skola INNER JOIN mesto ON mesto.id=skola.mesto INNER join pocet_prijatych ON pocet_prijatych.skola = skola.id INNER JOIN obor ON obor.id = pocet_prijatych.obor";
+            $result = $conn-> query($sql);
+            
+            if($result->num_rows > 0) {
+                while ($row = $result-> fetch_assoc()) {
+                    echo "<tr><td>" . $row["Škola"] . "</td><td>" . $row["Město"] . "</td><td>" . $row["Počet přijatých"] . "</td><td>" . $row["Obor"] ;
+                }
+            }
+            else {
+            echo "No results";
                 
-                
-    </div>
-         
-     </div>
-    
-        <?php } ?>
-  </div>
+            }
+            $conn-> close();
+            
+            ?>
+        </table>
+        </div>
+        <div>
+            <p></p>
+        </div>
     </body>
 </html>
--- application/views/pages/mapa.php
+++ application/views/pages/mapa.php
@@ -1,19 +1,41 @@
 <html>
     <head>
-        <title>Domovská stránka</title>
+        <title>Mapa</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+        <style>
+            #map {
+                width: 100%;
+                height: 100vh;
+            }
+            
+        </style>
+            
+    
+    
     </head>
     <body>
         
         
+        <div id="map"> 
         
-        <p>Mapa</p>
-    <p>&nbsp</p>
+        </div>
         
     </body>
 </html>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+<script>
+    var map = L.map('map').setView([49.065596, 17.480848], 11);
+
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map)
+
+L.marker([49.065596, 17.480848]).addTo(map);
+</script>
